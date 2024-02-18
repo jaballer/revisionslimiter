97e341499bd3cc741729b77890a80b0c2b1578a7
jaballer <jtorres@activecampaign.com>
2024-02-17 17:50:03 -0800

    updated EntrySaveListener

--- src/listeners/EntrySaveListener.php
+++ src/listeners/EntrySaveListener.php
@@ -3,13 +3,25 @@ namespace jaballer\revisionslimiter\listeners;
 
 use Craft;
 use craft\events\ElementEvent;
+use craft\models\Entry;
 use yii\base\Event;
 
 class EntrySaveListener
 {
     public static function handleBeforeSaveElement(ElementEvent $event)
     {
-        // Your logic to limit revisions goes here
-        // For example, you can check the number of existing revisions and delete old ones if the limit is reached
+        $element = $event->element;
+        
+        // Check if the element is an Entry
+        if ($element instanceof Entry) {
+            // Get the current number of revisions for the entry
+            $revisionsCount = Craft::$app->getEntryRevisions()->getCountByEntryId($element->id);
+
+            // If the number of revisions is greater than or equal to 10, delete the oldest revisions
+            if ($revisionsCount >= 10) {
+                $revisionsToDelete = $revisionsCount - 9; // Keep the latest 10 revisions
+                Craft::$app->getEntryRevisions()->deleteDraftsByEntryId($element->id, $revisionsToDelete);
+            }
+        }
     }
 }
